f0d7c6e7ffc10bbf36b297a86ec3d9ada09762a1
Milton <milton.a.pastor.f@gmail.com>
2025-08-05 18:55:34 -0500

    add manifest grades_manager

---
+++ grades_manager/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Grades Manager',
+    'summary': 'Handles grades among students and courses',
+    'description': 'Handles grades among students and courses',
+    'author': 'Milton',
+    'category': 'Base',
+    'version': '17.0.0.1',
+    'depends': ['base'],
+    'data': [],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
